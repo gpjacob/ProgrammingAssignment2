26356438c6d755989638b068ef34d99cc02f8e0d
gpjacob <gpjacob@gmail.com>
2015-08-23 22:44:41 +0530

    Solution to assignment 2
    Solution for matrix inversion assignment 2 as part of R programming on
    Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code takes advantage of the scoping rules of the R language 
+## to create an efficient matrix invertor for different invertible matrices
 
-## Write a short comment describing this function
+
+## This function will store a list of functions that 
+## 1. Get: Returns the matrix passed as input
+## 2. Set: Change the input matrix
+## 3. setinverse: stores the value of the inverse matrix
+## 4. getinverse: returns value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
-
-## Write a short comment describing this function
+## This function checks against memory if the inverse is previously stored and if so
+## returns is otherwise it calculates the inverse and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+c=rbind(c(1, -1/4), c(-1/4, 1))
+g<-makeCacheMatrix(c)
+cacheSolve(g)
+
+d<-rbind(c(4,3), c(3,2))
+
+h<-makeCacheMatrix(d)
+cacheSolve(h)
+cacheSolve(g)
+
